ef51caaac3c4bae92c767a20e8902a04f4f57756
remorses <beats.by.morse@gmail.com>
2023-01-09 12:58:17 +0100

    middleware:

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     let res = NextResponse.next()
     if (!req.nextUrl.pathname.startsWith('/api')) {
-        res.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate')
+        res.headers.set('Cache-Control', 'public, s-maxage=1, stale-while-revalidate')
     }
     return res
 }
