91a9a3cb201b52a1f3a27601a7892366b4c61246
remorses <beats.by.morse@gmail.com>
2023-01-09 12:39:19 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
     let res = await NextResponse.next(req)
     if (!req.nextUrl.pathname.startsWith('/api')) {
         res.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate')
