d3778f4cd89b94e00c536accc7dbd8bc22b977a3
remorses <beats.by.morse@gmail.com>
2023-01-09 12:22:48 +0100

    added api route

---
+++ pages/api/hello.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+
+export default function handler(req) {
+    const res = NextResponse.json({ hello: 'world' })
+    res.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate')
+    return res
+}
+
+export const config = {
+    runtime: 'edge',
+}
